1c77e6b732d3b3127c7be759a48306ae1b8e133f
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2020-04-24 14:45:30 +0200

    Login supposedly work

--- app/src/main/java/dk/easv/ATForum/LoginActivity.java
+++ app/src/main/java/dk/easv/ATForum/LoginActivity.java
@@ -1,16 +1,64 @@
 package dk.easv.ATForum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.forum.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "XYZ";
+    EditText loginEmail, loginPassword;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.txtEmail);
+        loginPassword = findViewById(R.id.txtPassword);
+
+        Button loginSubmit = findViewById(R.id.btnLogin);
+
+        loginSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        String emailString = loginEmail.getText().toString();
+        String passwordString = loginPassword.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Login successful " + task.getResult().getUser().getEmail());
+
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error on login", e);
+            }
+        });
     }
 }
