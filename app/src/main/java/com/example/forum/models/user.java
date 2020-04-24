8c4ddf913bbfbedbad5b9ac93c28b8eb9234d5a6
Asvør <asvor_rasmussen@hotmail.com>
2020-04-24 12:41:26 +0200

    Update user.java

--- app/src/main/java/com/example/forum/models/user.java
+++ app/src/main/java/com/example/forum/models/user.java
@@ -1,25 +1,25 @@
 package com.example.forum.models;
 
 public class User {
-private String uid;
-private String username;
-private String name;
-private String email;
-private String photoURL;
-private String role;
+        private String uid;
+        private String username;
+        private String name;
+        private String email;
+        private String photoURL;
+        private String role;
 
-    public User(String username, String name, String email, String role) {
-        this.username = username;
-        this.name = name;
-        this.email = email;
-        this.role = role;
-    }
+        public User(String username, String name, String email, String role) {
+            this.username = username;
+            this.name = name;
+            this.email = email;
+            this.role = role;
+        }
 
-    public String getPhotoURL() {
-        return photoURL;
-    }
+        public String getPhotoURL() {
+            return photoURL;
+        }
 
-    public void setPhotoURL(String photoURL) {
-        this.photoURL = photoURL;
+        public void setPhotoURL(String photoURL) {
+            this.photoURL = photoURL;
+        }
     }
-}
