4324965e01e727b353e24f07c950037b44ba5903
Asvør <asvor_rasmussen@hotmail.com>
2020-04-24 12:27:54 +0200

    Update user.java

--- app/src/main/java/models/user.java
+++ app/src/main/java/models/user.java
@@ -1,4 +1,12 @@
 package models;
 
 public class user {
+private String uid;
+private String username;
+private String name;
+private String email;
+private String photoURL;
+private String role;
+
+
 }
