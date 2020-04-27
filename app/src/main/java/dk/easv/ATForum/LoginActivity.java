40e65dcaad878cf12e2f436201cdbe6b008749f7
Asvør <asvor_rasmussen@hotmail.com>
2020-04-27 13:16:15 +0200

    hide password

--- app/src/main/java/dk/easv/ATForum/LoginActivity.java
+++ app/src/main/java/dk/easv/ATForum/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -38,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
 
         loginEmail = findViewById(R.id.txtEmail);
         loginPassword = findViewById(R.id.txtPassword);
+        loginPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
 
         Button loginSubmit = findViewById(R.id.btnLogin);
 
