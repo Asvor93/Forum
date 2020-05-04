5977fd0b4505b0a142ffe89f18f9ac3140ce8181
Asvør <asvor_rasmussen@hotmail.com>
2020-05-04 13:22:18 +0200

    mainActivity options menu created

--- app/src/main/java/dk/easv/ATForum/MainActivity.java
+++ app/src/main/java/dk/easv/ATForum/MainActivity.java
@@ -1,11 +1,17 @@
 package dk.easv.ATForum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.forum.R;
 
@@ -21,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
         Button btnLoginMain = findViewById(R.id.btnMainLogin);
         btnLoginMain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,18 +51,55 @@ public class MainActivity extends AppCompatActivity {
                 startProfileActivity();
             }
         });
+        */
+
     }
 
+    // Creates the options menu.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu1, menu);
+        return true;
+    }
+
+    // Handles menu selection.
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.profileMenu:
+                Intent intent = new Intent(this, ProfileActivity.class);
+                intent.putExtra("user", currentUser);
+                startActivityForResult(intent, 3);
+                return true;
+            case R.id.loginMenu:
+                Intent loginIntent = new Intent(this, LoginActivity.class);
+                startActivityForResult(loginIntent, 1);
+                return true;
+            case R.id.signUpMenu:
+                Intent signUpIntent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivityForResult(signUpIntent, 2);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /*
     private void startProfileActivity() {
         Intent intent = new Intent(this, ProfileActivity.class);
         intent.putExtra("user", currentUser);
         startActivityForResult(intent, 3);
     }
 
+     */
+
+    /*
     private void startLoginActivity() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivityForResult(intent, 1);
     }
+
+     */
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -66,8 +110,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
     private void startSignUpActivity() {
         Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
         startActivityForResult(intent, 2);
     }
+     */
 }
