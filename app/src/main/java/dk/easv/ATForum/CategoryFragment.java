3bea5ff704900a6acb0dbe3be09cc1afc0f3002f
Asvør <asvor_rasmussen@hotmail.com>
2020-05-12 11:13:55 +0200

    category fragment created

---
+++ app/src/main/java/dk/easv/ATForum/CategoryFragment.java
@@ -0,0 +1,33 @@
+package dk.easv.ATForum;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
+
+import com.example.forum.R;
+
+
+public class CategoryFragment extends ListFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/dk/easv/ATForum/PostActivity.java
@@ -0,0 +1,17 @@
+package dk.easv.ATForum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.example.forum.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
