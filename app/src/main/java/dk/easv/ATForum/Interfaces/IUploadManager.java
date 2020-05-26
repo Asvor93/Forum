0cb3dea1ccfef8afb0b7a7abb7f88ea655e5d2d8
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2020-05-26 10:42:14 +0200

    Update IUploadManager.java

--- app/src/main/java/dk/easv/ATForum/Interfaces/IUploadManager.java
+++ app/src/main/java/dk/easv/ATForum/Interfaces/IUploadManager.java
@@ -4,10 +4,11 @@ import android.widget.ImageView;
 
 public interface IUploadManager {
 
+    // Callback for the url result
     interface IONResult {
             void onResult(String res);
     }
 
-
+    // Uploads a picture to the database and sets the image to the img view
     void uploadPicture(ImageView img, IONResult callback);
 }
