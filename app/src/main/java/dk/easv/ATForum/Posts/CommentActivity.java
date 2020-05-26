a33f7646a89bfe8aa8ca9b15890f70ab468c85c8
Asvør <asvor_rasmussen@hotmail.com>
2020-05-26 10:48:09 +0200

    create notifications almost working

--- app/src/main/java/dk/easv/ATForum/Posts/CommentActivity.java
+++ app/src/main/java/dk/easv/ATForum/Posts/CommentActivity.java
@@ -1,6 +1,8 @@
 package dk.easv.ATForum.Posts;
 
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,9 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.example.forum.R;
 import com.r0adkll.slidr.Slidr;
@@ -19,6 +24,7 @@ import dk.easv.ATForum.DataAccessFactory;
 import dk.easv.ATForum.Interfaces.IDataAccess;
 import dk.easv.ATForum.MenuActivity;
 import dk.easv.ATForum.Models.Comment;
+import dk.easv.ATForum.Notifications;
 
 
 public class CommentActivity extends MenuActivity {
@@ -27,12 +33,15 @@ public class CommentActivity extends MenuActivity {
     private CommentAdapter commentAdapter;
     private List<Comment> commentList;
     private ListView commentListView;
+    private NotificationManagerCompat notificationManagerCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
         Slidr.attach(this);
+
+        notificationManagerCompat = NotificationManagerCompat.from(this);
 
         commentListView = findViewById(R.id.comList);
         GetExtras();
@@ -43,6 +52,7 @@ public class CommentActivity extends MenuActivity {
                 commentList = comments;
                 commentAdapter = new CommentAdapter(CommentActivity.this, R.layout.comment_cell, comments);
                 commentListView.setAdapter(commentAdapter);
+                sendNotification(commentListView);
             }
         });
 
@@ -67,6 +77,22 @@ public class CommentActivity extends MenuActivity {
                 goToCreateComment();
             }
         });
+    }
+
+    public void  sendNotification(View v) {
+
+        Intent activityIntent = new Intent(this, CommentActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, Notifications.channelId1)
+                .setSmallIcon(R.drawable.ic_comment)
+                .setContentTitle("New Comment")
+                // .setContentText()
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentIntent(contentIntent)
+                .build();
+        notificationManagerCompat.notify(1, notification);
     }
 
     private void goToCreateComment() {
